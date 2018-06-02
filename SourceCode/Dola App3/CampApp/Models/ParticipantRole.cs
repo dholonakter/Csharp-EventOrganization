6759330f018a1d34bc4e2c8c0452a6a45dbba4ce
Dholon Akter <dholonakter721@gmail.com>
2018-06-02 12:24:16 +0200

    Camp Registeration is working

---
+++ SourceCode/Dola App3/CampApp/Models/ParticipantRole.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CampApp.Models
+{
+    public enum ParticipantRole
+    {
+        None,
+        Leader,
+        Member
+    }
+}
